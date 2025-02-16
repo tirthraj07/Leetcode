3cd0113a3d3fdf4c57fa6f96df03d77876af4434
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2025-02-16 18:03:24 +0530

    Linked List Cycle

--- Linked List/Linked List Cycle/Main.java
+++ Linked List/Linked List Cycle/Main.java
@@ -3,3 +3,37 @@ public class Main {
         System.out.println("Hello World");
     }
 }
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    @SuppressWarnings("all")
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null) {
+            if (slow == fast)
+                return true;
+
+            slow = slow.next;
+            if (fast.next == null)
+                return false;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+}
